7c01eb09eade4b0d6c7a1353c23a65ba5ceaf777
Nguyễn Thành Tài <100697516+7ai23@users.noreply.github.com>
2024-09-30 20:11:15 +0700

    fix2

--- src/fetchData/User.js
+++ src/fetchData/User.js
@@ -1,12 +1,5 @@
 import axios from "./axios";
 
-const getProfile = () => {
-  return axios.get("/get-user-by-id");
-};
-const createNewUser = (userData) => {
-  return axios.post("/create-new-user", userData);
-};
-
 const handleSetDataUserDetail = (userData) => {
   return axios.post(`/setDataUserDetail`, userData);
 };
@@ -15,4 +8,4 @@ const getUsersById = (id) => {
   return axios.post(`/getUserById?id${id}`);
 };
 
-export { getProfile, createNewUser, handleSetDataUserDetail, getUsersById };
+export { handleSetDataUserDetail, getUsersById };
