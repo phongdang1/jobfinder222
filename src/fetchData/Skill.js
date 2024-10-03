94815298d57c5cf4ea5a5d06a1e50a067e2684c3
dongnguyen <dongntde160034@fpt.edu.vn>
2024-10-03 21:08:29 +0700

    UpdateFileSkill

---
+++ src/fetchData/Skill.js
@@ -0,0 +1,23 @@
+import axios from "./axios";
+
+const getAllSkillWithLimit = (userData, limit, offset) => {
+    return axios.get(`getAllSkillWithLimit?limit=${limit}&offset=${offset}`, userData)
+}
+
+const handleCreateNewSkill = (userData) => {
+    return axios.post("createNewSkill", userData)
+}
+
+const handleUpdateSkill = (userData) => {
+    return axios.post("updateSkill", userData)
+}
+
+const handleDeleteSkill = (userData) => {
+    return axios.post("/deleteSkill", userData)
+}
+
+const getAllSkillByCategory = (categoryJobCode) => {
+  return axios.get(`getAllSkillByCategory?categoryJobCode=${categoryJobCode}`);
+};
+
+export {getAllSkillWithLimit, handleCreateNewSkill, handleUpdateSkill, handleDeleteSkill, getAllSkillByCategory};
