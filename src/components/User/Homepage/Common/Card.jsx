0959c01abf213f37a978144d3f10d95472b29797
Nguyễn Thành Tài <100697516+7ai23@users.noreply.github.com>
2024-11-28 17:58:43 +0700

    Pagination

--- src/components/User/Homepage/Common/Card.jsx
+++ src/components/User/Homepage/Common/Card.jsx
@@ -23,96 +23,95 @@ function JobCard({ expand, data }) {
     console.log(id);
     navigate(`job-detail/${id}`);
   };
-  const date = new Date()
+  const date = new Date();
 
   return (
     <>
       {data && data.length > 0 ? (
-        data.map(
-          (card, index) =>
-            card.isHot === 1 && date < new Date(card.timeEnd) ? ( // Đặt điều kiện đúng cách
-              <div key={index}>
-                <Card
-                  className={`border-none w-full rounded-lg hover:bg-[#E6E6FA]/50 group hover:outline-2 hover:outline-primary cursor-pointer ${
-                    card.isHot === 1
-                      ? "bg-primary/20 hover:bg-violet-200"
-                      : "bg-white"
-                  }`}
-                >
-                  {card.isHot === 1 && (
-                    <span className="absolute top right-0 bg-orange-600 text-white text-sm font-semibold px-2 py-1 rounded-tl-md rounded-bl-md">
-                      <WhatshotIcon className="text-[#ffdd85] mr-2" />
-                      SUPER HOT
-                      <span className="absolute bottom-0 right-0 w-0 h-0 border-l-8 border-r-8 border-t-8 border-transparent border-t-orange-600 transform rotate-90 translate-x-1 translate-y-1"></span>
-                    </span>
-                  )}
-                  <CardBody>
-                    <div className="flex gap-8 items-center justify-start w-full">
-                      <div
-                        onClick={() => handleNavigate(card.id)}
-                        className="relative bg-transparent shrink-0"
-                      >
-                        <Image
-                          alt="Album cover"
-                          className="object-cover rounded-lg"
-                          height={90}
-                          shadow="md"
-                          src={
-                            card.userPostData?.userCompanyData?.thumbnail
-                              ? card.userPostData?.userCompanyData?.thumbnail
-                              : "https://nextui.org/images/album-cover.png"
-                          }
-                          width={90}
-                        />
-                      </div>
+        data.map((card, index) =>
+          card.isHot === 1 && date < new Date(card.timeEnd) ? ( // Đặt điều kiện đúng cách
+            <div key={index}>
+              <Card
+                className={`border-none w-full rounded-lg hover:bg-[#E6E6FA]/50 group hover:outline-2 hover:outline-primary cursor-pointer ${
+                  card.isHot === 1
+                    ? "bg-primary/20 hover:bg-violet-200"
+                    : "bg-white"
+                }`}
+              >
+                {card.isHot === 1 && (
+                  <span className="absolute top right-0 bg-orange-600 text-white text-sm font-semibold px-2 py-1 rounded-tl-md rounded-bl-md">
+                    <WhatshotIcon className="text-[#ffdd85] mr-2" />
+                    SUPER HOT
+                    <span className="absolute bottom-0 right-0 w-0 h-0 border-l-8 border-r-8 border-t-8 border-transparent border-t-orange-600 transform rotate-90 translate-x-1 translate-y-1"></span>
+                  </span>
+                )}
+                <CardBody>
+                  <div className="flex gap-8 items-center justify-start w-full">
+                    <div
+                      onClick={() => handleNavigate(card.id)}
+                      className="relative bg-transparent shrink-0"
+                    >
+                      <Image
+                        alt="Album cover"
+                        className="object-cover rounded-lg"
+                        height={90}
+                        shadow="md"
+                        src={
+                          card.userPostData?.userCompanyData?.thumbnail
+                            ? card.userPostData?.userCompanyData?.thumbnail
+                            : "https://nextui.org/images/album-cover.png"
+                        }
+                        width={90}
+                      />
+                    </div>
 
-                      <div className="flex flex-col w-full">
-                        <TooltipProvider>
-                          <Tooltip>
-                            <TooltipTrigger className="text-start">
-                              <p
-                                onClick={() => handleNavigate(card.id)}
-                                className="text-base font-medium group-hover:text-primary w-fit hover:underline hover:underline-offset-2"
-                              >
-                                {card.userPostData.userCompanyData.name}
-                              </p>
-                            </TooltipTrigger>
-                            <TooltipContent
-                              side={index % 3 === 0 ? "right" : "left"}
+                    <div className="flex flex-col w-full">
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger className="text-start">
+                            <p
+                              onClick={() => handleNavigate(card.id)}
+                              className="text-base font-medium group-hover:text-primary w-3/5 max-w-xs whitespace-nowrap hover:underline hover:underline-offset-2 text-ellipsis overflow-hidden"
                             >
-                              <TooltipBox id={card.id} />
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
+                              {card.userPostData.userCompanyData.name}
+                            </p>
+                          </TooltipTrigger>
+                          <TooltipContent
+                            side={index % 3 === 0 ? "right" : "left"}
+                          >
+                            <TooltipBox id={card.id} />
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
 
-                    <p className="font-normal text-base text-gray-500">
-                      {card.postDetailData.name}
-                    </p>
-                    <div className="flex mt-2 -ml-1 items-center relative w-full space-x-2">
-                      <Badge
-                        variant="outline"
-                        className="bg-white w-fit text-nowrap rounded-lg"
-                      >
-                        {card.postDetailData.salaryTypePostData.value}
-                      </Badge>
-                      <Badge
-                        variant="outline"
-                        className="bg-white w-fit text-nowrap rounded-lg"
-                      >
-                        {card.postDetailData.provincePostData.value}
-                      </Badge>
+                      <p className="font-normal text-base text-gray-500">
+                        {card.postDetailData.name}
+                      </p>
+                      <div className="flex mt-2 -ml-1 items-center relative w-full space-x-2">
+                        <Badge
+                          variant="outline"
+                          className="bg-white w-fit text-nowrap rounded-lg"
+                        >
+                          {card.postDetailData.salaryTypePostData.value}
+                        </Badge>
+                        <Badge
+                          variant="outline"
+                          className="bg-white w-fit text-nowrap rounded-lg"
+                        >
+                          {card.postDetailData.provincePostData.value}
+                        </Badge>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </CardBody>
-            </Card>
-          </div>
-        ) : null
-      )
-    ) : (
-      <p>No data available</p> // Trường hợp không có dữ liệu
-    )}
-  </>
+                </CardBody>
+              </Card>
+            </div>
+          ) : null
+        )
+      ) : (
+        <p>No data available</p> // Trường hợp không có dữ liệu
+      )}
+    </>
   );
 }
 
